b7c5771f7b1ec8bae37102fd8ec6c2ef5dc6b178
lcorso1234 <lawrencecorso1@gmail.com>
2026-03-04 22:11:03 -0600

    Love8

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const productRoutes = require("./routes/products");
 const quoteRoutes = require("./routes/quotes");
 
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 const mongoUri =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/rumiwear";
 
